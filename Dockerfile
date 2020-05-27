e5d0065055d6257ed64f46f18d28cdcde424c870
snakehead007 <snakehead007@pm.me>
2020-05-27 16:55:08 +0200

    docker env fix

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,8 @@ COPY package*.json /usr/src/app/
 RUN npm install
 COPY . /usr/src/app
 EXPOSE 3000
-ENV TokenValue="${TokenValue}"
-ENV TokenSecret="${TokenSecret}"
-ENV ConsumerSecret="${ConsumerSecret}"
-ENV ConsumerKey="${ConsumerKey}"
-ENV StoreName="${StoreName}"
-ENV SellerName="${SellerName}"
+ENV TOKEN_VALUE="${TOKEN_VALUE}"
+ENV TOKEN_SECRET="${TOKEN_SECRET}"
+ENV CONSUMER_SECRET="${CONSUMER_SECRET}"
+ENV CONSUMER_KEY="${CONSUMER_KEY}"
 CMD [ "npm", "start" ]
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var OAuth = require('oauth');
 var oauth_data = require('../oauth');
 /* GET home page. */
 var oauth = new OAuth.OAuth(
-    process.env.TokenValue,
-    process.env.TokenSecret,
-    process.env.ConsumerKey,
-    process.env.ConsumerSecret,
+    process.env.TOKEN_VALUE,
+    process.env.TOKEN_SECRET,
+    process.env.CONSUMER_KEY,
+    process.env.CONSUMER_SECRET,
     oauth_data.oauth_version,
     null,
     oauth_data.oauth_signature_method
@@ -16,8 +16,8 @@ var oauth = new OAuth.OAuth(
 router.all('/:value', function(req, res, next) {
   oauth.get(
       'https://api.bricklink.com/api/store/v1/'+req.params.value,
-      process.env.TokenValue,
-      process.env.TokenSecret, //test user secret
+      process.env.TOKEN_VALUE,
+      process.env.TOKEN_SECRET, //test user secret
       function (e, data){
         if (e) console.error(e);
           res.setHeader('Content-Type', 'application/json');
@@ -27,8 +27,8 @@ router.all('/:value', function(req, res, next) {
 router.all('/:value/:value2', function(req, res, next) {
     oauth.get(
         'https://api.bricklink.com/api/store/v1/'+req.params.value+"/"+req.params.value2,
-        process.env.TokenValue,
-        process.env.TokenSecret, //test user secret
+        process.env.TOKEN_VALUE,
+        process.env.TOKEN_SECRET, //test user secret
         function (e, data){
             if (e) console.error(e);
             res.setHeader('Content-Type', 'application/json');
@@ -38,8 +38,8 @@ router.all('/:value/:value2', function(req, res, next) {
 router.all('/:value/:value2/:value3', function(req, res, next) {
     oauth.get(
         'https://api.bricklink.com/api/store/v1/'+req.params.value+"/"+req.params.value2+"/"+req.params.value3,
-        process.env.TokenValue,
-        process.env.TokenSecret, //test user secret
+        process.env.TOKEN_VALUE,
+        process.env.TOKEN_SECRET, //test user secret
         function (e, data){
             if (e) console.error(e);
             res.setHeader('Content-Type', 'application/json');
