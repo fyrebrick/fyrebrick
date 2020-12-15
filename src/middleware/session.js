f14d68d81cd5f91597b045cb7986feb75a4c9f94
snakehead007 <snakehead007@pm.me>
2020-12-15 22:26:54 +0100

    fixed sessions

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -2,16 +2,23 @@ const {logger} = require('../configuration/logger');
 
 const isSignedIn = (req,res,next) => {
     //TODO check if successful signed in
-    if(req.session.logged_in){ 
-        next();
-    }else{
-        if(req.session){
-            res.session.destroy((err)=>{
-                if(err){
-                    logger.error(`could not destory session ${err.message}`);
-                }
-            });
+    if(req.session){
+        if(req.session.logged_in){
+            next();
+        }else if(!req.session.logged_in){
+            logger.info(`Not logged in anymore, redirecting to homepage`);
+            try{
+                req.session.destroy((err)=>{
+                    if(err){
+                        logger.error(`Caught error on destroying session in callback`);
+                    }
+                });
+            }catch(err){
+                logger.error(`Caught error on destroying session`);
+            }
+            res.redirect('/');
         }
+    }else{
         res.redirect('/');
     }
 };
