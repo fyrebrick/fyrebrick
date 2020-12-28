1c290982dbe2eb7aa7bce9f7743241b434af5bb6
snakehead007 <snakehead007@pm.me>
2020-12-28 16:18:01 +0100

    first version of settings

---
+++ src/controllers/my/settings.js
@@ -0,0 +1,7 @@
+const settings = {
+    index: (req,res,next)=>{
+        res.render('settings');
+    }
+}
+
+module.exports = settings;
--- src/routes/my/settings.js
+++ src/routes/my/settings.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
+const settings = require('../../controllers/my/settings');
 
-router.get('/settings');
+router.get('/',settings.index);
 
 module.exports = router;
