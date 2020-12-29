50b2ebf4b7034be9ddfa145046720ba24d659d56
snakehead007 <snakehead007@pm.me>
2020-12-29 08:26:24 +0100

    update with ranking

--- src/routes/my/settings.js
+++ src/routes/my/settings.js
@@ -4,4 +4,6 @@ const settings = require('../../controllers/my/settings');
 router.get('/',settings.index);
 router.put('/bricklinkApi',settings.bricklinkApi.put);
 router.put('/inventoryInterval',settings.inventoryInterval.put);
+router.get('/update/orders',settings.update.orders);
+router.get('/update/inventory',settings.update.inventory);
 module.exports = router;
