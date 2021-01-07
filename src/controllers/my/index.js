35bc49d58b4b3ade87965154b94dfbc13ddb79b8
snakehead007 <snakehead007@pm.me>
2021-01-07 18:51:37 +0100

    registration help added

--- src/controllers/my/index.js
+++ src/controllers/my/index.js
@@ -3,16 +3,13 @@ const {User} = require("fyrebrick-helper").models;
 const bricklinkPlus = require("bricklink-plus");
 
 const dashboard = async (req,res,next) =>{
-    let order = await Order.findOne({consumer_key:req.session.user.CONSUMER_KEY});
     let info = {
-      n4totalLots: "#",
-      n4totalItems: "#",
-      n4totalViews: "#"
+      n4totalLots: "N/A",
+      n4totalItems: "N/A",
+      n4totalViews: "N/A"
     }
-    if(order){
-      if(!await bricklinkPlus.plus.maintanceCheck.monthly()){
-        info = await bricklinkPlus.plus.stores.getStoreStats(order.seller_name);
-      }
+    if(!await bricklinkPlus.plus.maintanceCheck.monthly()){
+      info = await bricklinkPlus.plus.stores.getStoreStats(req.session.user.userName);
     }
     const user = await User.findById(req.session._id);
     res.render("dashboard",{active:"dashboard",info:info,user:user});
